c8c9802c1972ab96556b78045055c905ce0a6fa1
Patrick Kranz <patrick-kranz@live.de>
2025-03-18 14:18:34 +0100

    …

--- update.sh
+++ update.sh
@@ -9,11 +9,11 @@ fi
 
 DATA_STORE="https://raw.githubusercontent.com/LokiMidgard/linux-settings/refs/heads/main"
 
-curl -o ~/update.sh $DATA_STORE/update.sh
-curl -o ~/.zshrc $DATA_STORE/.zshrc
-curl -o ~/.p10k.zsh $DATA_STORE/.p10k.zsh
-curl -o ~/.zsh_custom $DATA_STORE/.zsh_custom
-curl -o ~/update-certificates.sh $DATA_STORE/update-certificates.sh
+curl  -fsSL -o ~/update.sh $DATA_STORE/update.sh
+curl  -fsSL -o ~/.zshrc $DATA_STORE/.zshrc
+curl  -fsSL -o ~/.p10k.zsh $DATA_STORE/.p10k.zsh
+curl  -fsSL -o ~/.zsh_custom $DATA_STORE/.zsh_custom
+curl  -fsSL -o ~/update-certificates.sh $DATA_STORE/update-certificates.sh
 
 # update the certificates
 chmod +x ~/update-certificates.sh
