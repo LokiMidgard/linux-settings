a9761f8a2c4b820ab15a2854cc7df0b5d8eb3a95
Patrick Kranz <patrick-kranz@live.de>
2025-04-17 22:07:38 +0200

    allow download of certificate if certificate is not trusted
    after all we want to install it

--- update-certificates.sh
+++ update-certificates.sh
@@ -1,7 +1,7 @@
 # check if the file is downloadable from https://step.fritz.box/certs/root_ca.crt   and download it to /usr/local/share/ca-certificates as new_root_ca.crt
-if curl -s --head  --request GET https://step.fritz.box/certs/root_ca.crt | grep "200 OK" > /dev/null; then
+if curl -s --head --insecure --request GET https://step.fritz.box/certs/root_ca.crt | grep "200 OK" > /dev/null; then
     echo "File is downloadable"
-    sudo curl -o /usr/local/share/ca-certificates/new_root_ca.crt https://step.fritz.box/certs/root_ca.crt
+    sudo curl --insecure -o /usr/local/share/ca-certificates/new_root_ca.crt https://step.fritz.box/certs/root_ca.crt
     # check if the new file is different from the old one
     if ! cmp -s /usr/local/share/ca-certificates/new_root_ca.crt /usr/local/share/ca-certificates/root_ca.crt; then
         echo "File is different"
