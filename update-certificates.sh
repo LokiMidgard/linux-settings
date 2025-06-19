fc399f9c37d158d2de025175af2fbd201a653c86
Patrick Kranz <patrick-kranz@live.de>
2025-06-19 19:35:35 +0200

    …

--- update-certificates.sh
+++ update-certificates.sh
@@ -1,18 +1,18 @@
 # check if the file is downloadable from https://step.fritz.box/certs/root_ca.crt   and download it to /usr/local/share/ca-certificates as new_root_ca.crt
 if curl -s --head --insecure --request GET https://step.fritz.box/certs/root_ca.crt | grep "HTTP/2 200" > /dev/null; then
-    echo "File is downloadable"
+    echo "Zertifikat https://step.fritz.box/certs/root_ca.crt ist herunterladbar."
     sudo curl --insecure -o /usr/local/share/ca-certificates/new_root_ca.crt https://step.fritz.box/certs/root_ca.crt
     # check if the new file is different from the old one
     if ! cmp -s /usr/local/share/ca-certificates/new_root_ca.crt /usr/local/share/ca-certificates/root_ca.crt; then
-        echo "File is different"
+        echo "Neues Zertifikat unterscheidet sich von /usr/local/share/ca-certificates/root_ca.crt. Zertifikat wird aktualisiert."
         # update the certificate
         sudo cp /usr/local/share/ca-certificates/new_root_ca.crt /usr/local/share/ca-certificates/root_ca.crt
         sudo update-ca-certificates
     else
-        echo "File is the same"
+        echo "Neues Zertifikat ist identisch mit /usr/local/share/ca-certificates/root_ca.crt. Keine Aktualisierung notwendig."
     fi
     # remove the new file
     sudo rm /usr/local/share/ca-certificates/new_root_ca.crt
 else
-    echo "File is not downloadable"
+    echo "Zertifikat https://step.fritz.box/certs/root_ca.crt konnte nicht heruntergeladen werden."
 fi
