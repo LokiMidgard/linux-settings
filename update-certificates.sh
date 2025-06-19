7d5852dc92d68c6155739fbe001313e7cd4ab7ad
Patrick Kranz <patrick-kranz@live.de>
2025-06-19 19:39:52 +0200

    Improve certificate download command with additional flags for better reliability

--- update-certificates.sh
+++ update-certificates.sh
@@ -1,7 +1,7 @@
 # check if the file is downloadable from https://step.fritz.box/certs/root_ca.crt   and download it to /usr/local/share/ca-certificates as new_root_ca.crt
-if curl -s --head --insecure --request GET https://step.fritz.box/certs/root_ca.crt | grep "HTTP/2 200" > /dev/null; then
+if curl -fsSL --head --insecure --request GET https://step.fritz.box/certs/root_ca.crt | grep "HTTP/2 200" > /dev/null; then
     echo "Zertifikat https://step.fritz.box/certs/root_ca.crt ist herunterladbar."
-    sudo curl --insecure -o /usr/local/share/ca-certificates/new_root_ca.crt https://step.fritz.box/certs/root_ca.crt
+    sudo curl -fsSL --insecure -o /usr/local/share/ca-certificates/new_root_ca.crt https://step.fritz.box/certs/root_ca.crt
     # check if the new file is different from the old one
     if ! cmp -s /usr/local/share/ca-certificates/new_root_ca.crt /usr/local/share/ca-certificates/root_ca.crt; then
         echo "Neues Zertifikat unterscheidet sich von /usr/local/share/ca-certificates/root_ca.crt. Zertifikat wird aktualisiert."
